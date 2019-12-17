9df17ec588a6fbc58f60fffd710f150d0977c9fb
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-12-17 13:51:59 +0630

    initial commit

---
+++ src/main/java/com/solt/bootonlinestore/BootOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.solt.bootonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootOnlineStoreApplication.class, args);
+    }
+
+}
